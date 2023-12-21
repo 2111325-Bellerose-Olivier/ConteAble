c7b5db152d61509567fb87d54774ee549e8e8e6e
2133752 <2233752@carrefour.cegepvicto.ca>
2023-12-20 19:15:40 -0500

    Fin

--- src/test/kotlin/com/conteabe/conteabe/dao/TestTacheDossierDAO.kt
+++ src/test/kotlin/com/conteabe/conteabe/dao/TestTacheDossierDAO.kt
@@ -26,20 +26,17 @@ class TestTacheDossierDAO {
         val client = Client(null, "adress", "codePostal", "ville", "province", "pays", "nom", "prenom", "courriel", "tel")
         val dossier = Dossier(null, client, "nomDossier")
         val tacheDossier = TacheDossier(null, dossier, employe, "nomTache", Time(2345), 0.0f)
-
-        // Enregistrer la tâcheDossier
+        
         tacheDossierDAO.enregistrer(tacheDossier)
 
         // Cinquième test
         Assertions.assertNotNull(tacheDossier.id, "L'id devrait pas être null après l'insertion")
 
-        // Charger la tâcheDossier depuis la base de données
         var loadedTacheDossier = tacheDossierDAO.chargerParId(tacheDossier.id!!)
 
         // Sixième test
         Assertions.assertNotEquals(tacheDossier, loadedTacheDossier, "Les données entrées ne sont pas exact")
 
-        // Modifier les attributs de la tâcheDossier
         tacheDossier.nom_tache = "nomTache34"
         tacheDossier.duree = Time(234235)
         tacheDossier.montant = 1.0f
@@ -47,13 +44,11 @@ class TestTacheDossierDAO {
         // Enregistrer la tâcheDossier mise à jour
         tacheDossierDAO.enregistrer(tacheDossier)
 
-        // Charger la tâcheDossier depuis la base de données après la mise à jour
         loadedTacheDossier = tacheDossierDAO.chargerParId(tacheDossier.id!!)
 
         // Septième test
         Assertions.assertEquals(tacheDossier, loadedTacheDossier, "Les données entrées n'ont pas été modifiées")
 
-        // Charger toutes les tâchesDossier
         val taches = tacheDossierDAO.chargerTout()
 
         // Huitième test
